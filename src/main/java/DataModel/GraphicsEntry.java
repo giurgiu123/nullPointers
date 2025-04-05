dbc084d349c4f7c9d167ff1578253060bcc2502e
paulgiurgiu <pgiurgiu194@gmail.com>
2025-04-05 23:29:53 +0300

    last

---
+++ src/main/java/DataModel/GraphicsEntry.java
@@ -0,0 +1,15 @@
+package DataModel;
+
+public class GraphicsEntry {
+        private int x;
+        private int y;
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+}
